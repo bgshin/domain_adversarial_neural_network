7c93ede41695f5e227f35ce1f32da1675fb0df8b
Bonggun Shin <nomolos79@gmail.com>
2017-02-23 15:49:20 -0500

    model done

--- src/demo.py
+++ src/demo.py
@@ -87,7 +87,7 @@ def load_amazon(source_name, target_name, data_folder=None):
 #
 #     return 2 * (1. - 2 * best_risk)
 
-def train(xs_train, ys_train, xt_train, yt_train, x_dev, y_dev):
+def train(xs_train, ys_train, xt_train, yt_train, xs_dev, ys_dev, xt_dev, yt_dev):
 
     #Model Hyperparameters
     tf.flags.DEFINE_integer("embedding_dim", 5000, "Dimensionality of character embedding (default: 5000")
@@ -96,7 +96,7 @@ def train(xs_train, ys_train, xt_train, yt_train, x_dev, y_dev):
 
     # Training parameters
     tf.flags.DEFINE_integer("batch_size", 32, "Batch Size (default: 64)")
-    tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
+    tf.flags.DEFINE_integer("num_epochs", 10, "Number of training epochs (default: 200)")
     tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
     tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
 
@@ -114,145 +114,136 @@ def train(xs_train, ys_train, xt_train, yt_train, x_dev, y_dev):
     graph = tf.get_default_graph()
     with graph.as_default():
         model = DANN(
-                sequence_length=xs_train.shape[1],
                 num_classes= ys_train.shape[1],
                 embedding_size=FLAGS.embedding_dim,
-                batch_size=FLAGS.batch_size)
+                hidden_layer_size=50)
 
         global_step = tf.Variable(0, name="global_step", trainable=False)
-        optimizer = tf.train.AdamOptimizer(FLAGS.learning_rate)
-        grads_and_vars = optimizer.compute_gradients(model.total_loss)
-        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 
-        # Keep track of gradient values and sparsity
-        grad_summaries = []
-        for g, v in grads_and_vars:
-            if g is not None:
-                grad_hist_summary = tf.histogram_summary("{}/grad/hist".format(v.name), g)
-                sparsity_summary = tf.scalar_summary("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
-                grad_summaries.append(grad_hist_summary)
-                grad_summaries.append(sparsity_summary)
-        grad_summaries_merged = tf.merge_summary(grad_summaries)
+        # train_a = tf.train.GradientDescentOptimizer(0.1).minimize(loss_a, var_list=[A])
+        # train_b = tf.train.GradientDescentOptimizer(0.1).minimize(loss_b, var_list=[B])
 
-        # Summaries for loss and accuracy
-        label_pred_loss_summary = tf.scalar_summary("mean_label_pred_loss", model.mean_label_pred_loss)
-        label_pred_acc_summary = tf.scalar_summary("label_pred_accuracy", model.label_pred_accuracy)
+        train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="vars")
+        var_list_classifier = train_vars[0:4]
+        var_list_discriminator = train_vars[4:]
 
-        domain_pred_loss_summary = tf.scalar_summary("mean_domain_pred_loss", model.mean_domain_pred_loss)
-        domain_pred_acc_summary = tf.scalar_summary("domain_pred_accuracy", model.domain_pred_accuracy)
+        # op_classifier = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(model.total_cost,
+        #                                                                      var_list=var_list_classifier)
+        # op_discriminator = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(model.total_neg_cost,
+        #                                                                      var_list=var_list_discriminator)
 
-        # Train Summaries
-        train_summary_op = tf.merge_summary([label_pred_loss_summary,
-                                             domain_pred_loss_summary,
-                                             label_pred_acc_summary,
-                                             domain_pred_acc_summary,
-                                             grad_summaries_merged])
-        train_summary_dir = os.path.join(out_dir, "summaries", "train")
+        op_classifier = tf.train.AdamOptimizer(FLAGS.learning_rate)
+        op_discriminator = tf.train.AdamOptimizer(FLAGS.learning_rate)
 
-        # Dev summaries
-        dev_summary_op = tf.merge_summary([label_pred_loss_summary,
-                                           domain_pred_loss_summary,
-                                           label_pred_acc_summary,
-                                           domain_pred_acc_summary,
-                                           ])
-        dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
+        grad_classifier = op_classifier.compute_gradients(model.total_cost, var_list=var_list_classifier)
+        grad_discriminator = op_discriminator.compute_gradients(model.total_neg_cost, var_list=var_list_discriminator)
+
+        train_op_classifier = op_classifier.apply_gradients(grad_classifier, global_step=global_step)
+        train_op_discriminator = op_discriminator.apply_gradients(grad_discriminator, global_step=global_step)
 
         # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
         checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
         checkpoint_prefix = os.path.join(checkpoint_dir, "model")
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
-        saver = tf.train.Saver(tf.all_variables())
-
+        # saver = tf.train.Saver(tf.all_variables())
+        saver = tf.train.Saver(tf.global_variables())
 
 
         with tf.Session(graph=graph) as sess:
-            train_summary_writer = tf.train.SummaryWriter(train_summary_dir, graph)
-            dev_summary_writer = tf.train.SummaryWriter(dev_summary_dir, graph)
+            tf.global_variables_initializer().run()
 
-            def train_step(x_batch, y_batch, batch_domain_labels, l, lr):
+            def train_step(xs_batch, ys_batch, xt_batch, ds_batch, dt_batch, lam):
                 """
                 A single training step
                 """
                 feed_dict = {
-                    model.input_x: x_batch,
-                    model.input_y: y_batch,
-                    model.domain: batch_domain_labels,
-                    model.adapt_lambda: l,
+                    model.xs: xs_batch,
+                    model.ys: ys_batch,
+                    model.xt: xt_batch,
+                    model.domain_s: ds_batch,
+                    model.domain_t: dt_batch,
+                    model.adapt_lambda: lam,
                 }
-                _, step, summaries, batch_loss, ploss, dloss, p_acc, d_acc = sess.run(
-                    [train_op, global_step, train_summary_op, model.total_loss, model.mean_label_pred_loss,
-                     model.mean_domain_pred_loss, model.label_pred_accuracy, model.domain_pred_accuracy], feed_dict)
+                _, _, step, classifier_loss, discriminator_source_loss, discriminator_target_loss, total_cost, \
+                classifier_acc, discriminator_acc = sess.run(
+                    [train_op_classifier, train_op_discriminator, global_step, model.classifier_loss,
+                     model.discriminator_source_loss, model.discriminator_target_loss, model.total_cost,
+                     model.classifier_acc, model.discriminator_acc], feed_dict)
                 time_str = datetime.datetime.now().isoformat()
                 print(
-                "{}: step {}, batch_loss {:g}, dloss {:g}, ploss {:g}, dp_acc {:g}, lp_acc {:g}".format(time_str, step,
-                                                                                                      batch_loss, dloss,
-                                                                                                      ploss, d_acc, p_acc))
-                train_summary_writer.add_summary(summaries, step)
+                "{}: step {}, c_loss {:g}, ds_loss {:g}, dt_loss {:g}, loss{:g}, c_acc {:g}, d_acc {:g}".format(
+                    time_str, step, classifier_loss, discriminator_source_loss, discriminator_target_loss, total_cost,
+                    classifier_acc, discriminator_acc))
 
-            def dev_step(x_batch, y_batch, batch_domain_labels, l, writer=None):
+            def dev_step(xs_batch, ys_batch, xt_batch, ds_batch, dt_batch, lam):
                 """
                 Evaluates model on a dev set
                 """
                 feed_dict = {
-                    model.input_x: x_batch,
-                    model.input_y: y_batch,
-                    model.domain: batch_domain_labels,
-                    model.adapt_lambda: l,
-
-                    # model.dropout_keep_prob: 1.0
+                    model.xs: xs_batch,
+                    model.ys: ys_batch,
+                    model.xt: xt_batch,
+                    model.domain_s: ds_batch,
+                    model.domain_t: dt_batch,
+                    model.adapt_lambda: lam,
                 }
-                step, summaries, batch_loss, dloss, ploss, p_acc, d_acc = sess.run(
-                    [global_step, dev_summary_op, model.total_loss, model.mean_label_pred_loss,
-                     model.mean_domain_pred_loss, model.label_pred_accuracy, model.domain_pred_accuracy], feed_dict)
+                step, classifier_loss, discriminator_source_loss, discriminator_target_loss, total_cost, \
+                classifier_acc, discriminator_acc = sess.run(
+                    [global_step, model.classifier_loss, model.discriminator_source_loss,
+                     model.discriminator_target_loss, model.total_cost,
+                     model.classifier_acc, model.discriminator_acc], feed_dict)
                 time_str = datetime.datetime.now().isoformat()
                 print(
-                "{}: step {}, batch_loss {:g}, dloss {:g}, ploss {:g}, d_acc {:g}, p_acc {:g}".format(time_str, step,
-                                                                                                      batch_loss, dloss,
-                                                                                                      ploss, d_acc,
-                                                                                                      p_acc))
-                if writer:
-                    writer.add_summary(summaries, step)
+                    "{}: step {}, c_loss {:g}, ds_loss {:g}, dt_loss {:g}, loss{:g}, c_acc {:g}, d_acc {:g}".format(
+                        time_str, step, classifier_loss, discriminator_source_loss, discriminator_target_loss,
+                        total_cost,
+                        classifier_acc, discriminator_acc))
 
+            # tf.initialize_all_variables().run()
 
-            tf.initialize_all_variables().run()
 
             gen_source_batch = batch_generator([xs_train, ys_train], FLAGS.batch_size / 2)
             gen_target_batch = batch_generator([xt_train, yt_train], FLAGS.batch_size / 2)
 
-            domain_labels = np.vstack(
-                [np.tile([1., 0.], [FLAGS.batch_size / 2, 1]), np.tile([0., 1.], [FLAGS.batch_size / 2, 1])])
+            domain_labels_s = np.tile([1., 0.], [FLAGS.batch_size / 2, 1])
+            domain_labels_t = np.tile([0., 1.], [FLAGS.batch_size / 2, 1])
+            # domain_labels = np.vstack(
+            #     [np.tile([1., 0.], [FLAGS.batch_size / 2, 1]), np.tile([0., 1.], [FLAGS.batch_size / 2, 1])])
 
             if ((len(xs_train) + len(xt_train)) % FLAGS.batch_size > 0):
                 num_batches = ((len(xs_train) + len(xt_train)) / FLAGS.batch_size) + 1
             else:
                 num_batches = ((len(xs_train) + len(xt_train)) / FLAGS.batch_size)
 
-            for i in range(num_batches):
-                p = float(i) / num_batches
-                l = 2. / (1. + np.exp(-10. * p)) - 1
-                lr = 0.01 / (1. + 10 * p) ** 0.75
+            for epoch in range(FLAGS.num_epochs):
+                for i in range(num_batches):
+                    p = float(i) / num_batches
+                    l = 2. / (1. + np.exp(-10. * p)) - 1
+                    lr = 0.01 / (1. + 10 * p) ** 0.75
 
-                X0, y0 = gen_source_batch.next()
-                X1, y1 = gen_target_batch.next()
-                X = np.vstack([X0, X1])
-                Y = np.vstack([y0, y1])
+                    Xs, Ys = gen_source_batch.next()
+                    Xt, Yt = gen_target_batch.next()
 
-                train_step(X, Y, domain_labels, l, lr)
-                current_step = tf.train.global_step(sess, global_step)
-                if current_step % FLAGS.evaluate_every == 0:
-                    d_labels = np.vstack(
-                [np.tile([1., 0.], [len(y_dev) / 2, 1]), np.tile([0., 1.], [len(x_dev) / 2, 1])])
-                    print("\nEvaluation:")
-                    dev_step(x_dev, y_dev, d_labels, l, writer=dev_summary_writer)
-                    print("")
-                if current_step % FLAGS.checkpoint_every == 0:
-                    path = saver.save(sess, checkpoint_prefix, global_step=current_step)
-                    print("Saved model checkpoint to {}\n".format(path))
+                    train_step(Xs, Ys, Xt, domain_labels_s, domain_labels_t, l)
+                    current_step = tf.train.global_step(sess, global_step)
+                    if current_step % FLAGS.evaluate_every == 0:
+                        ds_label = np.tile([1., 0.], [len(ys_dev), 1])
+                        dt_label = np.tile([1., 0.], [len(yt_dev), 1])
 
+                        print("\nEvaluation:")
+                        dev_step(xs_dev, ys_dev, xt_dev, ds_label, dt_label, l)
+                        print("")
+                    if current_step % FLAGS.checkpoint_every == 0:
+                        path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                        print("Saved model checkpoint to {}\n".format(path))
+
+
+def test(xtest, ytest_hot):
+    print 'd'
 
 def main():
-    data_folder = './data/' # where the datasets are
+    data_folder = '../data/' # where the datasets are
     source_name = 'books'   # source domain: books, dvd, kitchen, or electronics
     target_name = 'dvd'     # traget domain: books, dvd, kitchen, or electronics
 
@@ -260,26 +251,38 @@ def main():
     ##Loading pre-embedded data
     print("Loading data ...")
     xs, ys, xt, yt, xtest, ytest = load_amazon(source_name, target_name, data_folder)
+    print xs.shape
+    print xt.shape
 
     #Define validation sets
     nb_valid = int(0.1 * len(ys))
-    x_valid, y_valid = xs[-nb_valid:, :], ys[-nb_valid:]
-    xs, ys = xs[0:-nb_valid, :], ys[0:-nb_valid]
+    nb_valid_s = nb_valid / 2
+    nb_valid_t = nb_valid / 2
+
+    # x_valid, y_valid = xs[-nb_valid:, :], ys[-nb_valid:]
+
+    ys_hot = np.zeros((len(ys), 2))
+    ys_hot[np.arange(len(ys)), ys] = 1
+    yt_hot = np.zeros((len(yt), 2))
+    yt_hot[np.arange(len(yt)), yt] = 1
+
+    x_valid_s, y_valid_s_hot = xs[-nb_valid_s:, :], ys_hot[-nb_valid_s:, :]
+    x_valid_t, y_valid_t_hot = xt[-nb_valid_s:, :], yt_hot[-nb_valid_s:, :]
+
+    xs, ys = xs[0:-nb_valid_s, :], ys[0:-nb_valid_s]
+    xt, yt = xt[0:-nb_valid_s, :], yt[0:-nb_valid_s]
     print xs.shape
-    ys_hot = np.zeros((len(ys),2))
-    ys_hot[ys] = 1
-    yt_hot = np.zeros((len(yt),2))
-    yt_hot[yt] = 1
-    y_valid_hot = np.zeros((len(y_valid),2))
-    y_valid_hot[y_valid] = 1
-    ytest_hot = np.zeros((len(ytest),2))
-    ytest_hot[ytest] = 1
+    print xt.shape
+
+    ytest_hot = np.zeros((len(ytest), 2))
+    ytest_hot[np.arange(len(ytest)), ytest] = 1
 
 
     print("Training...")
-    train(xs, ys_hot, xt, yt_hot, x_valid, y_valid_hot)
+    train(xs, ys_hot, xt, yt_hot, x_valid_s, y_valid_s_hot, x_valid_t, y_valid_t_hot)
 
     print("Evaluating...")
+    # test(xtest, ytest_hot)
 
 
     #
--- src/tfDANN.py
+++ src/tfDANN.py
@@ -4,6 +4,109 @@ import numpy as np
 from flip_gradient import flip_gradient
 
 class DANN(object):
+    def __init__(self, num_classes, embedding_size, hidden_layer_size):
+        self.xs = tf.placeholder(tf.int32, [None, embedding_size], name="x_source")
+        self.ys = tf.placeholder(tf.float32, [None, num_classes], name="y_source")
+        self.xt = tf.placeholder(tf.float32, [None, embedding_size], name="x_target")
+        self.domain_s = tf.placeholder(tf.float32, [None, 2], name="domain_label_source")
+        self.domain_t = tf.placeholder(tf.float32, [None, 2], name="domain_label_target")
+        self.adapt_lambda = tf.placeholder(tf.float32, [])
+
+        self.xs = tf.cast(self.xs, tf.float32)
+        self.xt = tf.cast(self.xt, tf.float32)
+
+        print 'self.xs.get_shape()', self.xs.get_shape()
+        print 'self.xt.get_shape()', self.xt.get_shape()
+        print 'self.domain_s.get_shape()', self.domain_s.get_shape()
+        print 'self.domain_t.get_shape()', self.domain_t.get_shape()
+
+        with tf.variable_scope('vars'):
+            W = tf.Variable(tf.truncated_normal([embedding_size, hidden_layer_size], stddev=0.1), name='W')
+            b = tf.Variable(tf.truncated_normal([hidden_layer_size], stddev=0.1), name='b')
+            # b = tf.constant(0.1, shape=[hidden_layer_size])
+            V = tf.Variable(tf.truncated_normal([hidden_layer_size, num_classes], stddev=0.1), name='V')
+            c = tf.Variable(tf.truncated_normal([num_classes], stddev=0.1), name='c')
+            # c = tf.constant(0.1, shape=[num_classes])
+            U = tf.Variable(tf.truncated_normal([hidden_layer_size, num_classes], stddev=0.1), name='U')
+            d = tf.Variable(tf.truncated_normal([num_classes], stddev=0.1), name='d')
+            # d = tf.constant(0.1, shape=[num_classes])
+
+        # with tf.variable_scope('Feature_Extractor'):
+        #     W = tf.Variable(tf.truncated_normal([embedding_size, hidden_layer_size], stddev=0.1))
+        #     b = tf.constant(0.1, shape=[hidden_layer_size])
+        #
+        # with tf.variable_scope('Classifier'):
+        #     V = tf.Variable(tf.truncated_normal([hidden_layer_size, num_classes], stddev=0.1))
+        #     c = tf.constant(0.1, shape=[num_classes])
+        #
+        # with tf.variable_scope('Domain_Discriminator'):
+        #     U = tf.Variable(tf.truncated_normal([hidden_layer_size, num_classes], stddev=0.1))
+        #     d = tf.constant(0.1, shape=[num_classes])
+
+        # with tf.variable_scope('Classifier_Path'):
+        # with tf.variable_scope('Feature_Extractor', reuse=True), tf.variable_scope('Classifier', reuse=True):
+        # W = tf.get_variable("W", [embedding_size, hidden_layer_size])
+        # b = tf.get_variable("b", [ hidden_layer_size])
+        # V = tf.get_variable("V", [hidden_layer_size, num_classes])
+        # c = tf.get_variable("c", [num_classes])
+
+        self.feature_xs = tf.nn.sigmoid(tf.matmul(self.xs, W) + b)
+        print 'self.feature_xs.get_shape()', self.feature_xs.get_shape()
+        self.GyGfXs = tf.nn.softmax(tf.matmul(self.feature_xs, V) + c)
+        print 'self.GyGfXs.get_shape()', self.GyGfXs.get_shape()
+
+
+        # with tf.variable_scope('Domain_Discriminator_Path'):
+        # with tf.variable_scope('Feature_Extractor', reuse=True), tf.variable_scope('Domain_Discriminator', reuse=True):
+        # W = tf.get_variable("W", [embedding_size, hidden_layer_size])
+        # b = tf.get_variable("b", [hidden_layer_size])
+        # U = tf.get_variable("U", [hidden_layer_size, num_classes])
+        # d = tf.get_variable("d", [num_classes])
+
+        self.GdGfXs = tf.nn.softmax(tf.matmul(self.feature_xs, U) + d)
+        print 'self.GdGfXs.get_shape()', self.GdGfXs.get_shape()
+
+        self.feature_xt = tf.nn.sigmoid(tf.matmul(self.xt, W) + b)
+        print 'self.feature_xt.get_shape()', self.feature_xt.get_shape()
+        self.GdGfXt = tf.nn.softmax(tf.matmul(self.feature_xt, U) + d)
+        print 'self.GdGfXt.get_shape()', self.GdGfXt.get_shape()
+
+        self.classifier_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.GyGfXs, self.ys))
+        self.discriminator_source_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.GdGfXs, self.domain_s))
+        self.discriminator_target_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.GdGfXt, self.domain_t))
+        self.total_cost = self.classifier_loss - \
+                          self.adapt_lambda* (self.discriminator_source_loss+self.discriminator_target_loss)
+
+        self.total_neg_cost = -self.total_cost
+
+        self.classifier_predictions = tf.equal(tf.argmax(self.ys, 1), tf.argmax(self.GyGfXs, 1))
+        self.classifier_acc = tf.reduce_mean(tf.cast(self.classifier_predictions, "float"),
+                                                   name="classifier_acc")
+
+        self.discriminator_predictions_s = tf.equal(tf.argmax(self.domain_s, 1), tf.argmax(self.GdGfXs , 1))
+        self.discriminator_accuracy_s = tf.reduce_mean(tf.cast(self.discriminator_predictions_s, "float"),
+                                                   name="discriminator_accuracy_s")
+
+        print 'self.domain_t', self.domain_t
+        print 'self.GdGfXt', self.GdGfXt
+
+        self.discriminator_predictions_t = tf.equal(tf.argmax(self.domain_t, 1), tf.argmax(self.GdGfXt, 1))
+        self.discriminator_accuracy_t = tf.reduce_mean(tf.cast(self.discriminator_predictions_t, "float"),
+                                                       name="discriminator_accuracy_t")
+
+        self.discriminator_acc = self.discriminator_accuracy_s + self.discriminator_accuracy_t
+
+        print 'self.classifier_loss', self.classifier_loss
+        print 'self.discriminator_source_loss', self.discriminator_source_loss
+        print 'self.discriminator_target_loss', self.discriminator_target_loss
+        print 'self.total_cost', self.total_cost
+        print 'self.total_neg_cost', self.total_neg_cost
+
+        print 'self.classifier_acc', self.classifier_acc
+        print 'self.discriminator_acc', self.discriminator_acc
+
+
+class DANNold(object):
     """
     A CNN for text classification.
     Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
@@ -71,6 +174,7 @@ class DANN(object):
 
             domain_predictions = tf.nn.softmax(dp_logits)
             print(domain_predictions.get_shape())
+
         with tf.name_scope("domain_predictor_loss"):
             domain_pred_losses = tf.nn.softmax_cross_entropy_with_logits(domain_predictions, self.domain)
             self.mean_domain_pred_loss = tf.reduce_mean(domain_pred_losses) #+ l2_reg_lambda * l2_loss
